eac4ebf04c0652c089cbca332875dabbc946ed47
Eric Lowry <ericmichaellowry@gmail.com>
2020-04-18 12:21:25 -0700

    Updated Header component to render a container for all header elements, and an h2 with a bootstrap badge to display the average grade

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
 
 function Header(props) {
+  const { avg } = props;
   return (
-    <h1 className="mt-4">Student Grade Table</h1>
+    <div className="header d-flex justify-content-between align-items-center mt-4">
+      <h1>Student Grade Table</h1>
+      <h2>
+        Average Grade
+        <span className="badge badge-secondary ml-2">{avg}</span>
+      </h2>
+    </div>
+
   );
 }
 
