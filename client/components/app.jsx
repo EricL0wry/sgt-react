f1eb8bf543b038e63fd002112f8416ad1cfabdb9
Eric Lowry <ericmichaellowry@gmail.com>
2020-04-18 11:07:43 -0700

    Added componentDidMount method to App and set up fetch request to get grades. Added grade and grade table components and set up table skeleton in JSX

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -7,6 +7,25 @@ function Header(props) {
   );
 }
 
+function Grade(props) {
+
+  return (
+    <tr></tr>
+  );
+}
+
+function GradeTable(props) {
+
+  return (
+    <table>
+      <thead></thead>
+      <tbody>
+        <Grade />
+      </tbody>
+    </table>
+  );
+}
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -15,10 +34,19 @@ class App extends React.Component {
     };
   }
 
+  componentDidMount() {
+
+    fetch('./api/grades')
+      .then(response => response.json())
+      .then(grades => this.setState({ grades }))
+      .catch(error => console.error('Unable to retrieve grades:', error));
+  }
+
   render() {
     return (
       <div className="container">
         <Header />
+        <GradeTable grades={this.state.grades}/>
       </div>
     );
   }
